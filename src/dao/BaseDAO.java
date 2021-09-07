8b960adac2f56cc4d768f8bd8c24cd191e43e121
nakamurakihiro <nakamurakihiro@gmail.com>
2021-09-07 21:48:14 +0900

    [add]DAO基底クラスの新規作成
    既存DAOクラスのDB関連フィールドを基底クラスとして新規作成

---
+++ src/dao/BaseDAO.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *@author Akihiro Nakamura
+ *DAO基底クラス
+ */
+public class BaseDAO {
+
+	/**
+	 *特定のデータベースとの接続
+	 */
+	protected Connection con;
+
+	/**
+	 *SQL文の解析と実行
+	 */
+	protected PreparedStatement ps;
+
+	/**
+	 *SQL実行結果の取得
+	 */
+	protected ResultSet rs;
+
+	/**
+	 *@throws SQLException データベース接続処理でエラー
+	 *データベースと接続するメソッド
+	 */
+	public void dbConnect() throws SQLException {
+		ConnectionManager cm = new ConnectionManager();
+		con = cm.connect();
+	}
+
+	/**
+	 *@throws SQLException データベース切断処理でエラー
+	 *データベースとの接続を切断するメソッド
+	 */
+	public void dbClose() throws SQLException {
+		ConnectionManager cm = new ConnectionManager();
+		cm.close(con);
+	}
+}
