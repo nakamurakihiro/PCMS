1a731fb5f3ec4ee7a35c5bea7f6e623cfc320718
nakamurakihiro <nakamurakihiro@gmail.com>
2021-09-11 21:23:48 +0900

    [update]JS 確認メッセージの更新
    削除確認メッセージ メソッド名修正
    工数記録保存確認メッセージの追加

--- WebContent/JavaScript/function.js
+++ WebContent/JavaScript/function.js
@@ -1,8 +1,18 @@
 
 /*削除確認*/
-function Check(){
-	var checked = confirm("削除してもよろしいですか？");
-		if (checked == true) {
+function Delete_Check(){
+	var deleted = confirm("削除してもよろしいですか？");
+		if (deleted == true) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+/*工数記録保存確認*/
+function Save_Check(){
+	var saved = confirm("工数記録を保存してもよろしいですか？");
+		if (saved == true) {
 			return true;
 		}else{
 			return false;
